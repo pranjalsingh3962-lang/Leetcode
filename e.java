95df8994138bb640c957b60b8bc0a4c003bb243a
Pranjal Singh <pranjalsingh3962@gmail.com>
2026-08-29 18:12:55 +0200

    Added leetcode solutions

---
+++ e.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int total=0;
+        int left=0;
+        for(int i=0;i<nums.length;i++){
+            total=total+nums[i];
+        }
+        for(int i=0;i<nums.length;i++){
+            int right=total-left-nums[i];
+            if(left==right){
+                return i;
+            }
+            left=left+nums[i];
+        }
+        return -1;
+    }}
+   
